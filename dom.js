312dc67131c6be1462fb75213b419ab7d410490e
Moheuddin Sehab <moheuddinsehab0@gmail.com>
2019-06-14 12:53:46 +0600

    changing few things

--- dom.js
+++ dom.js
@@ -2,14 +2,10 @@
 
 console.dir(document);
 
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
+//console.dir(document.URL);
+//document.title = "Hello world";
+//console.log(document.all[10]);
+//document.all[15].textContent = "my bad";
 // console.log(document.all[10]);
 // // document.all[10].textContent = 'Hello';
 // console.log(document.forms[0]);
