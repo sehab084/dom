673d194cf3be7272d532d4787ede7f3c6045e3fe
Moheuddin Sehab <moheuddinsehab0@gmail.com>
2019-06-14 15:25:25 +0600

    before adding click to change style button

--- dom.js
+++ dom.js
@@ -10,85 +10,49 @@
 // // document.all[10].textContent = 'Hello';
 
 // GETELEMENTBYID //
-var headerTitle = document.getElementById("header-title");
-var header = document.getElementById("main-header");
-header.style.borderBottom = "solid 2px #003";
-//console.log(headerTitle);
-//console.log(headerTitle.innerText);
-headerTitle.innerHTML = "<h3>This is innerHTML</h3>";
-headerTitle.style.borderTop = "solid 1px #fff";
-var iteams = document.getElementsByClassName("list-group-item");
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// header.style.borderBottom = "solid 2px #003";
+// //console.log(headerTitle);
+// //console.log(headerTitle.innerText);
+// headerTitle.innerHTML = "<h3>This is innerHTML</h3>";
+// headerTitle.style.borderTop = "solid 1px #fff";
+// var iteams = document.getElementsByClassName("list-group-item");
 //console.log(iteams);
-console.log(iteams[1]);
-iteams[1].textContent = "testing";
-// var headerTitle = document.getElementById('header-title');
-// var header = document.get    ElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom = 'solid 3px #000';
+//console.log(iteams[1]);
+//iteams[3].style.fontWeight = "bold";
 
-// GETELEMENTSBYCLASSNAME //
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < items.length; i++){
-//   items[i].style.backgroundColor = '#f4f4f4';
+// for (var i = 0; i < iteams.length; i++) {
+//   iteams[i].style.fontWeight = "bold";
+//   console.log(i);
 // }
+//query selector
+// var header = document.querySelector("#main-header");
+// console.log(header);
 
-// GETELEMENTSBYTAGNAME //
-// var li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent = 'Hello 2';
-// li[1].style.fontWeight = 'bold';
-// li[1].style.backgroundColor = 'yellow';
+// var lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
 
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
+// var changeSecondTitle = document.querySelectorAll(".title");
+// console.log(changeSecondTitle[1]);
+// changeSecondTitle[1].textContent = "make me";
 
-// for(var i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
-// }
+//making things stand out by changing odd
+var odd = document.querySelectorAll("li:nth-child(odd)");
 
-// QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
+for (var i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "#f2f2f2";
+}
 
-// var input = document.querySelector('input');
-// input.value = 'Hello World'
+var newDiv = document.createElement("div");
+var newText = document.createTextNode("Hello World");
+newDiv.appendChild(newText);
 
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
 
-// var item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
+container.insertBefore(newDiv, h1);
 
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
+console.log(newDiv);
 
-// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
-
-// QUERYSELECTORALL //
-// var titles = document.querySelectorAll('.title');
-
-// console.log(titles);
-// titles[0].textContent = 'Hello';
-
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even= document.querySelectorAll('li:nth-child(even)');
-
-// for(var i = 0; i < odd.length; i++){
-//   odd[i].style.backgroundColor = '#f4f4f4';
-//   even[i].style.backgroundColor = '#ccc';
-// }
+c;
